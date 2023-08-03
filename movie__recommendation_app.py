3fca1e49e0690bbfea88a5e4db88ab2697dcb047
bhavana-v-r <114577424+bhavana-v-r@users.noreply.github.com>
2023-08-03 17:13:37 +0530

    Update movie__recommendation_app.py

--- movie__recommendation_app.py
+++ movie__recommendation_app.py
@@ -17,22 +17,14 @@ def recommend_table(list_of_movie_enjoyed,tfidf_data, movie_count=20):
     :param movie_count: no of movies to suggest
     :return: dataframe containing suggested movie
     """
-    movie_enjoyed_df =
-    tfidf_data.reindex(list_of_movie_enjoyed)
+    movie_enjoyed_df = tfidf_data.reindex(list_of_movie_enjoyed)
     user_prof = movie_enjoyed_df.mean()
-    tfidf_subset_df =
-    tfidf_data.drop(list_of_movie_enjoyed)
-    similarity_array =
-    cosine_similarity(user_prof.values.reshape(1, -1),
-    tfidf_subset_df)
-    similarity_df =
-    pd.DataFrame(similarity_array.T,
+    tfidf_subset_df = tfidf_data.drop(list_of_movie_enjoyed)
+    similarity_array = cosine_similarity(user_prof.values.reshape(1, -1), tfidf_subset_df)
+    similarity_df = pd.DataFrame(similarity_array.T,
     index=tfidf_subset_df.index,
     columns=["similarity_score"])
-    sorted_similarity_df =
-    similarity_df.sort_values(by="similarity_score",
-    ascending=False).head(movie_count)
-    
+    sorted_similarity_df = similarity_df.sort_values(by="similarity_score", ascending=False).head(movie_count)
     return sorted_similarity_df
 def load_data():
     """
